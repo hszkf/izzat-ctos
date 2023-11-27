1091dfcdea3721692c9306718c356fbdef3a7b3a
hszkf <hasifzulkifli95@gmail.com>
2023-11-28 01:08:03 +0800

    up

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -62,90 +62,94 @@ const Home = async () => {
       </div>
 
       <div className="flex flex-col mt-4 gap-4">
-        {result.employees.map((employee: any) => (
-          <>
-            <Card key={employee.id}>
-              <CardHeader key={employee.id}>
-                <CardTitle key={employee.id}>
-                  <Avatar key={employee.id}>
-                    <AvatarImage src={employee.avatar} key={employee.id} />
-                    <AvatarFallback key={employee.id}>
-                      {employee.id}
-                    </AvatarFallback>
-                  </Avatar>
-                </CardTitle>
-                <CardDescription key={employee.id}>
-                  {employee.email}
-                </CardDescription>
-                <CardDescription key={employee.id}>
-                  <span className="text-black truncate text-sm font-medium leading-none mt-4">
-                    First Name :
-                  </span>{" "}
-                  {employee.first_name}
-                </CardDescription>
-                <CardDescription key={employee.id}>
-                  <span className="text-black truncate text-sm font-medium leading-none mt-1">
-                    Last Name :
-                  </span>{" "}
-                  {employee.last_name}
-                </CardDescription>
-                <CardDescription key={employee.id}>
-                  <span className="text-black truncate text-sm font-medium leading-none mt-1">
-                    Role :
-                  </span>{" "}
-                  {employee.role}
-                </CardDescription>
-                <CardDescription key={employee.id}>
-                  <span className="text-black truncate text-sm font-medium leading-none mt-1">
-                    Salary :
-                  </span>{" "}
-                  RM {employee.salary}
-                </CardDescription>
-              </CardHeader>
-              <CardFooter>
-                <div className="flex justify-start items-center gap-3">
-                  <Link
-                    className={buttonVariants({ variant: "outline" })}
-                    href="/employee"
-                  >
-                    Edit details
-                  </Link>
-                  <AlertDialog>
-                    <AlertDialogTrigger asChild>
-                      {/* <Button variant="destructive">Show Dialog</Button> */}
-                      {/* <Button
+        {result.employees.length > 0 ? (
+          result.employees.map((employee: any) => (
+            <>
+              <Card key={employee.id}>
+                <CardHeader key={employee.id}>
+                  <CardTitle key={employee.id}>
+                    <Avatar key={employee.id}>
+                      <AvatarImage src={employee.avatar} key={employee.id} />
+                      <AvatarFallback key={employee.id}>
+                        {employee.id}
+                      </AvatarFallback>
+                    </Avatar>
+                  </CardTitle>
+                  <CardDescription key={employee.id}>
+                    {employee.email}
+                  </CardDescription>
+                  <CardDescription key={employee.id}>
+                    <span className="text-black truncate text-sm font-medium leading-none mt-4">
+                      First Name :
+                    </span>{" "}
+                    {employee.first_name}
+                  </CardDescription>
+                  <CardDescription key={employee.id}>
+                    <span className="text-black truncate text-sm font-medium leading-none mt-1">
+                      Last Name :
+                    </span>{" "}
+                    {employee.last_name}
+                  </CardDescription>
+                  <CardDescription key={employee.id}>
+                    <span className="text-black truncate text-sm font-medium leading-none mt-1">
+                      Role :
+                    </span>{" "}
+                    {employee.role}
+                  </CardDescription>
+                  <CardDescription key={employee.id}>
+                    <span className="text-black truncate text-sm font-medium leading-none mt-1">
+                      Salary :
+                    </span>{" "}
+                    RM {employee.salary}
+                  </CardDescription>
+                </CardHeader>
+                <CardFooter>
+                  <div className="flex justify-start items-center gap-3">
+                    <Link
+                      className={buttonVariants({ variant: "outline" })}
+                      href="/employee"
+                    >
+                      Edit details
+                    </Link>
+                    <AlertDialog>
+                      <AlertDialogTrigger asChild>
+                        {/* <Button variant="destructive">Show Dialog</Button> */}
+                        {/* <Button
                         className="bg-red-600 hover:bg-red-400"
                         type="submit"
                         onClick={() => deleteEmployeeHandler(employee.id)}
                       >
                         Delete
                       </Button> */}
-                      <DeleteButton id={employee.id} />
-                    </AlertDialogTrigger>
-                    <AlertDialogContent>
-                      <AlertDialogHeader>
-                        <AlertDialogTitle>
-                          <span className="">Are you absolutely sure?</span>
-                        </AlertDialogTitle>
-                        <AlertDialogDescription>
-                          This action cannot be undone. This will permanently
-                          delete your account and remove your data from our
-                          servers.
-                        </AlertDialogDescription>
-                      </AlertDialogHeader>
-                      <AlertDialogFooter>
-                        <AlertDialogCancel>Cancel</AlertDialogCancel>
-                        <AlertDialogAction className="bg-red-600 hover:bg-red-400">
-                          Continue
-                        </AlertDialogAction>
-                      </AlertDialogFooter>
-                    </AlertDialogContent>
-                  </AlertDialog>
-                </div>
-              </CardFooter>
-            </Card>
-          </>
-        ))}
+                        <DeleteButton id={employee.id} />
+                      </AlertDialogTrigger>
+                      <AlertDialogContent>
+                        <AlertDialogHeader>
+                          <AlertDialogTitle>
+                            <span className="">Are you absolutely sure?</span>
+                          </AlertDialogTitle>
+                          <AlertDialogDescription>
+                            This action cannot be undone. This will permanently
+                            delete your account and remove your data from our
+                            servers.
+                          </AlertDialogDescription>
+                        </AlertDialogHeader>
+                        <AlertDialogFooter>
+                          <AlertDialogCancel>Cancel</AlertDialogCancel>
+                          <AlertDialogAction className="bg-red-600 hover:bg-red-400">
+                            Continue
+                          </AlertDialogAction>
+                        </AlertDialogFooter>
+                      </AlertDialogContent>
+                    </AlertDialog>
+                  </div>
+                </CardFooter>
+              </Card>
+            </>
+          ))
+        ) : (
+          <div>No employee here</div>
+        )}
       </div>
       {/* <div>{employees}</div> */}
     </>
